79c56c481ec020adf9d995c3ca7617baea457a73
peakpang <zengzhenxuan@mail.nwpu.edu.cn>
2026-03-22 22:40:49 +0800

    update eval.sh

--- experiments/UGP.kitti/eval.sh
+++ experiments/UGP.kitti/eval.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 python test.py --snapshot=/data/test04/code/UGP/ckpts/nuscenes.pth.tar --distance=10 # 20, 30, 40
+CUDA_VISIBLE_DEVICES=0 python test.py --snapshot=/Code/UGP/ckpts/kitti.pth.tar --distance=10 # 20, 30, 40
 CUDA_VISIBLE_DEVICES=0 python eval.py --method=lgr
 CUDA_VISIBLE_DEVICES=0 python eval.py --method=ransac --num_corr=50000
 
